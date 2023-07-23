e0eb68a338a6d73360564ca962c3dfd7e33c2c39
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2023-07-23 20:41:38 +0530

    Added PageElements of create account page. Initiated create account page objects class.

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,4 +1,15 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/functionalTests",
+        glue = {"stepDefinitions"},
+        //plugin = {"json:target/cucumber-reports/report.json"},
+        monochrome = true
+)
 public class TestRunner {
 }
